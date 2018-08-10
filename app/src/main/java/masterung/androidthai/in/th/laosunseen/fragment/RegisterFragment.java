2848cfbd4f7ac311416f7b62abbab954afa44f1d
masterUNG <phrombutr@gmail.com>
2018-08-10 14:38:31 +0700

    Start 10Augv5

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -163,7 +163,7 @@ public class RegisterFragment extends Fragment {
                 Toast.makeText(getActivity(), "Success Upload Photo", Toast.LENGTH_SHORT).show();
                 findPathUrlPhoto();
                 createPost();
-                createDatabase();
+
                 progressDialog.dismiss();
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -181,6 +181,8 @@ public class RegisterFragment extends Fragment {
     }   // uploadPhoto
 
     private void createDatabase() {
+
+        Log.d("10AugV1", "This is pathURL ==> " + pathURLString);
 
         UserModel userModel = new UserModel(uidString, nameString, emailString,
                 pathURLString, myPostString);
@@ -234,6 +236,7 @@ public class RegisterFragment extends Fragment {
                         public void onSuccess(Uri uri) {
                             urlStrings[0] = uri.toString();
                             pathURLString = urlStrings[0];
+                            createDatabase();
                             Log.d("9AugV1", "urlStrings[0] ==> " + urlStrings[0]);
                             Log.d("9AugV1", "pathURL ==> " + pathURLString);
                         }
